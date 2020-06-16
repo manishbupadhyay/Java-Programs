2536febc8e4b9d07d14f19341260d5d74feaa9e8
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-06-17 01:44:35 +0530

    Find Asci Char
    Find Asci Char
    
    Co-Authored-By: Manish Upadhyay <ermanishupadhyay@gmail.com>

---
+++ src/JavaPrograms/FindAsciChar.java
@@ -0,0 +1,21 @@
+package JavaPrograms;
+
+public class FindAsciChar {
+	public static void main(String[] args) {
+		char c = 'a'; // 97
+		char a = 'A'; //65
+		
+		int asci = c;
+		int asciA = a;
+		
+		int asciNumber = (int)c;
+		System.out.println(asci);
+		//or
+		System.out.println(asciNumber);
+		
+		int asciNumberA = (int)a;
+		System.out.println(asciA);
+		// or
+		System.out.println(asciNumberA);
+	}
+}
