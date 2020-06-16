343d79c6047d3dc2e18dea0aaa036af16738e84a
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-06-17 01:34:15 +0530

    Add Two Integer
    Add Two Integer
    
    Co-Authored-By: Manish Upadhyay <ermanishupadhyay@gmail.com>

---
+++ src/JavaPrograms/AddTwoInteger.java
@@ -0,0 +1,15 @@
+package JavaPrograms;
+
+public class AddTwoInteger {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 2;
+		int sum = a+b;
+		System.out.println("Addition of 2 integer is : " + sum);
+		
+		// there are many ways we can add 2 integer
+		System.out.println(10+2);
+	}
+
+}
