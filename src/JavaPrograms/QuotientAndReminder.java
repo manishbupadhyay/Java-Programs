0bd25d9124e87ff26f1610f09e4d1c39822dbb87
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-06-17 01:48:32 +0530

    Quotient And Reminder
    Quotient And Reminder
    
    Co-Authored-By: Manish Upadhyay <ermanishupadhyay@gmail.com>

---
+++ src/JavaPrograms/QuotientAndReminder.java
@@ -0,0 +1,18 @@
+package JavaPrograms;
+
+public class QuotientAndReminder {
+
+	public static void main(String[] args) {
+		
+		int dividend = 24;
+		int divisor = 4;
+		
+		int quotient = dividend / divisor;
+		int reminder = dividend % divisor;
+		
+		System.out.println("Quotient is : " + quotient);
+		System.out.println("Reminder is : " + reminder);
+
+	}
+
+}
