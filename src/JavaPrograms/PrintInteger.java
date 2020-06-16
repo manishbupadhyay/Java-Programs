fcda2211a40142c89757227238c2dea45f5f5597
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-06-17 01:31:11 +0530

    Print Integer
    Print Integer
    
    Co-Authored-By: Manish Upadhyay <ermanishupadhyay@gmail.com>

---
+++ src/JavaPrograms/PrintInteger.java
@@ -0,0 +1,14 @@
+package JavaPrograms;
+
+import java.util.Scanner;
+
+public class PrintInteger {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a interger number");
+		int num = scanner.nextInt();
+		System.out.println("You have entered: " + num);
+	}
+
+}
