6e75081be48dbba433a0ae88d733087443045626
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-06-17 01:38:50 +0530

    Multiplication of two floating point number
    Multiplication of two floating point number
    
    Co-Authored-By: Manish Upadhyay <ermanishupadhyay@gmail.com>

---
+++ src/JavaPrograms/MultiplyTwoFloatNumber.java
@@ -0,0 +1,11 @@
+package JavaPrograms;
+
+public class MultiplyTwoFloatNumber {
+
+	public static void main(String[] args) {
+		float f1 = 1.25f;
+		float f2 = 0.75f;
+		 
+		System.out.println("Multiplication of two floating point number is : " + (f1*f2));
+	}
+}
