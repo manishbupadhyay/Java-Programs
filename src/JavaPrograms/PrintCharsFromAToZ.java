089a1765fae5bb20afa268a85a06665e39c68a9c
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-06-28 23:41:07 +0530

    Print Chars From A To Z
    Print Chars From A To Z
    
    Co-Authored-By: Manish Upadhyay <ermanishupadhyay@gmail.com>

---
+++ src/JavaPrograms/PrintCharsFromAToZ.java
@@ -0,0 +1,17 @@
+package JavaPrograms;
+
+public class PrintCharsFromAToZ {
+
+	public static void main(String[] args) {
+		// print chars from A to Z
+		
+		for(char c = 'A'; c<='Z'; c++) {
+			//System.out.println(c + " ");
+			
+			// to print in same line
+			System.out.print(c + " ");
+		}
+
+	}
+
+}
