2ce23aa1c82f77573d9390cfcb823880e9369cf5
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-06-28 21:34:37 +0530

    Add simple way for Fibonacci Series
    Add simple way for Fibonacci Series
    
    Co-Authored-By: Manish Upadhyay <ermanishupadhyay@gmail.com>

--- src/JavaPrograms/FibonacciSeries.java
+++ src/JavaPrograms/FibonacciSeries.java
@@ -1,10 +1,23 @@
 package JavaPrograms;
 
 public class FibonacciSeries {
-	
+
 	static int n1=0,n2=1,n3=0;
-	
+
 	public static void main(String[] args) {
+
+		// simple way
+		int t1=0,t2=1,num=10;
+
+		for(int i=1; i<=num; i++) {
+			System.out.println(t1);
+			int sum = t1 + t2;
+			t1 = t2;
+			t2 = sum;
+		}
+		
+		System.out.println("--------------------------------");
+		
 		// Fibonnaci series is, next number is the sum of previous two numbers. example 0,1,1,2,3,5,8,13......
 		// Fibonacci series without using recursion
 		int a=0,b=1,c,count=10;
@@ -15,13 +28,13 @@ public class FibonacciSeries {
 			a=b;
 			b=c;
 		}
-		
+
 		System.out.println("Fibonacci series using recursion");
-		
+
 		fibonacciSeries(count-2);
 
 	}
-	
+
 	// Fibonacci series using recursion
 	static void fibonacciSeries(int count) {
 		if(count>0) {
