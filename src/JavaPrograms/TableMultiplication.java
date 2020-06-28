68ec2b7ab7c6b7ba830e0159b71aaf1018e22fed
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-06-28 21:02:11 +0530

    Table Multiplication
    Table Multiplication
    
    Co-Authored-By: Manish Upadhyay <ermanishupadhyay@gmail.com>

---
+++ src/JavaPrograms/TableMultiplication.java
@@ -0,0 +1,16 @@
+package JavaPrograms;
+
+public class TableMultiplication {
+
+	public static void main(String[] args) {
+		// table of 2 is 2 x 1 = 2
+		// 2 x 2 = 4
+		
+		int num = 2;
+		for(int i=1; i<=10; i++) {
+			System.out.println(num + " x " + i + " = " + (num*i));
+		}
+
+	}
+
+}
